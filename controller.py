868120c167a002bea8fe0cb75d467666108df9d1
Tom Proehl <proehlth@pm.me>
2021-11-11 13:19:59 +0100

    Update

--- controller.py
+++ controller.py
@@ -3,7 +3,7 @@ from pygame import color
 from pygame.constants import JOYAXISMOTION
 
 def my_square_animation():
-    my_square.x += motion[0] * 10
+    #my_square.x += motion[0] * 10
     my_square.y += motion[1] * 10
     if my_square.top <= 0:
         my_square.top = 0
@@ -42,6 +42,30 @@ while run:
                 motion[event.axis] = event.value
         elif event.type == pygame.JOYBUTTONDOWN:
             my_square_color = event.button
+        elif event.type == pygame.JOYHATMOTION:
+            if event.value == (-1,0):
+                motion[0] -= 1
+            if event.value == (0,1):
+                motion[1] -= 1
+            if event.value == (1,0):
+                motion[0] += 1
+            if event.value == (0,-1):
+                motion[1] += 1
+            if event.value == (-1,1):
+                motion[0] -= 1
+                motion[1] -= 1
+            if event.value == (-1,-1):
+                motion[0] -= 1
+                motion[1] += 1
+            if event.value == (1,-1):
+                motion[0] += 1
+                motion[1] += 1
+            if event.value == (1,1):
+                motion[0] += 1
+                motion[1] -= 1
+            if event.value == (0,0):
+                motion[0] = 0
+                motion[1] = 0
 
     my_square_animation()
     pygame.draw.rect(screen, colors[my_square_color], my_square)
